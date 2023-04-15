3ba22410502f92a44a93f7ebe02367a727fecbee
ANCHAL KUMAR <114271948+Anchal-pandey@users.noreply.github.com>
2023-04-15 16:45:43 +0530

    Add files via upload

---
+++ clock using HTML CSS/index.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+    date = new Date();
+    htime = d.gethour();
+    mtime = d.get.Minutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+    
+
+}, 1000);
